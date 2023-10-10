83d833ccce6636e5c507f90d0548f17e6ec2fb0b
LuisSilvaMoreno <link_61@hotmail.com>
2023-10-10 10:34:54 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const usersRouter = require('./routes/users');
+
+class Server{
+    constructor() {
+        this.app = express(); //Se instancia Express 
+        this.port = 3000;     // Definimos el puerto
+
+        //Paths     http://localhost:3000/api/v1
+        this.basePath = '/api/v1'; //Ruta Base
+        this.usersPath = `${this.basePath}/users`; //Path para la tabla Users
+
+        this.middlewares();   //Invocación de los middlewares
+
+        this.routes(); //Invocación de las rutas
+    }
+
+    middlewares() {
+        this.app.use(express.json()); //Para poder interpretar texto en formato JSON
+    }
+
+    routes() {
+        this.app.use(this.usersPath, usersRouter); //EndPoint de Users
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log("Server listening on port" + this.port)
+        });
+    }
+}
+
+module.exports = Server;
